334816e7ba703064a338cb8b3681ae4878d7232b
JaziriYoussef <youssefjaziri28@gmail.com>
2025-04-06 16:26:53 +0100

    tp3

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -9,10 +9,9 @@ use Symfony\Component\Routing\Annotation\Route;
 class IndexController extends AbstractController 
 {
     #[Route('/{name}', name: 'app_home', defaults: ['name' => 'World'])]
-    public function home(string $name): Response
+    public function home()
     {
-        return $this->render('index.html.twig', [
-            'name' => $name
-        ]);
+        $articles = ['Artcile1', 'Article 2','Article 3'];
+        return $this->render('articles/index.html.twig',['articles' => $articles]);
     }
 }
